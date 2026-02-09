c4c96771b11ced0a5204e1bf2cb3ee4f741e3317
abdaziznet <dev.abdaziz@outlook.com>
2026-02-09 11:14:28 +0100

    sugoi

--- task-2/redis_rest_api.py
+++ task-2/redis_rest_api.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from requests.auth import HTTPBasicAuth
 from config import BASE_URL, USERNAME, PASSWORD, HEADERS
@@ -7,38 +8,118 @@ auth = HTTPBasicAuth(USERNAME, PASSWORD)
 def create_database():
     payload = {
         "name": "exam-db",
-        "memory_size": 1024,
+        "type": "redis",
+        "memory_size": 1024 * 1024 * 1024,
         "shards_count": 1,
-        "replication": False,
-        "modules": []
+        "replication": False
     }
-    r = requests.post(f"{BASE_URL}/v1/bdbs", json=payload, headers=HEADERS, auth=auth)
+    r = requests.post(f"{BASE_URL}/v1/bdbs", json=payload, headers=HEADERS, auth=auth,  verify=False)
+
+    print("STATUS:", r.status_code)
+    print("RESPONSE:", r.text)
+    
     r.raise_for_status()
     return r.json()["uid"]
 
-def create_user(email, name, role):
+
+def create_new_user(email, name, role_uid):
     payload = {
         "email": email,
         "name": name,
-        "role": role,
-        "password": "TempPass123!"
+        "password": "TempPass123!",
+        "role_uids": [role_uid]
     }
-    requests.post(f"{BASE_URL}/v1/users", json=payload, headers=HEADERS, auth=auth)
+
+    r = requests.post(
+        f"{BASE_URL}/v1/users",
+        json=payload,
+        headers=HEADERS,
+        auth=auth,
+        verify=False
+    )
+
+    print(r.status_code, r.text)
+    r.raise_for_status()
 
 def list_users():
-    r = requests.get(f"{BASE_URL}/v1/users", auth=auth)
+    r = requests.get(f"{BASE_URL}/v1/users", auth=auth, verify=False)
     for u in r.json():
-        print(f"{u['name']} | {u['role']} | {u['email']}")
+        print(f"{u['name']} | {u['role_uids']} | {u['email']}")
+
+def wait_db_ready(db_uid, timeout=60):
+    print("Waiting DB to become active...")
+    start = time.time()
+
+    while time.time() - start < timeout:
+        r = requests.get(
+            f"{BASE_URL}/v1/bdbs/{db_uid}",
+            auth=auth,
+            verify=False
+        )
+        status = r.json()["status"]
+        print("DB status:", status)
+
+        if status == "active":
+            return
+
+        time.sleep(2)
+
+    raise TimeoutError("Database not ready in time")
+
+def create_role(db_uid, role_name, redis_acl):
+    payload = {
+        "name": role_name,
+        "management": "none",
+        "roles_permissions": [
+            {
+                "database_uid": db_uid,
+                "redis_acl": redis_acl
+            }
+        ]
+    }
+
+    r = requests.post(
+        f"{BASE_URL}/v1/roles",
+        json=payload,
+        headers=HEADERS,
+        auth=auth,
+        verify=False
+    )
+
+    print(f"CREATE ROLE [{role_name}]:", r.status_code)
+    if r.status_code >= 400:
+        print("ERROR:", r.text)
+
+    r.raise_for_status()
+    return r.json()["uid"]
+
 
 def delete_database(db_id):
-    requests.delete(f"{BASE_URL}/v1/bdbs/{db_id}", auth=auth)
+    requests.delete(f"{BASE_URL}/v1/bdbs/{db_id}", auth=auth,  verify=False)
 
 if __name__ == "__main__":
-    db_id = create_database()
+    db_uid = create_database()
 
-    create_user("john.doe@example.com", "John Doe", "db_viewer")
-    create_user("mike.smith@example.com", "Mike Smith", "db_member")
-    create_user("cary.johnson@example.com", "Cary Johnson", "admin")
+    wait_db_ready(db_uid, 60)
+
+    # Create roles
+    viewer_role = create_role(
+    db_uid,
+    "db_viewer",
+    "+@read -@write"
+    )
+
+    member_role = create_role(
+    db_uid,
+    "db_member",
+    "+@all"
+    )
+
+
+    # Create users
+    create_new_user("john.doe@example.com", "John Doe", viewer_role)
+    create_new_user("mike.smith@example.com", "Mike Smith", member_role)
+    create_new_user("cary.johnson@example.com", "Cary Johnson", 1)
 
     list_users()
-    delete_database(db_id)
+    delete_database(db_uid)
